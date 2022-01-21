e82cc3611261884adb1525e588f47b480b272472
Borys Więsek <lordboria.boryson@gmail.com>
2022-01-21 15:52:16 +0100

    Added scales model Added scales question, answers to viewmodel

---
+++ Skale_W_Praktyce/Skale_W_Praktyce/Models/ScaleAnswers.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace Skale_W_Praktyce.Models
+{
+    internal class ScaleAnswers : INotifyPropertyChanged
+    {
+
+        public ScaleAnswers()
+        {
+            //ScaleViewName = typeof(Scale);
+        }
+
+        #region Fields
+        private string questionAnswer;
+        private int questionAnswerPoints;
+        #endregion
+
+        #region Properties
+        public string QuestionAnswer
+        {
+            get { return questionAnswer; }
+            set
+            {
+                if (questionAnswer != value)
+                {
+                    questionAnswer = value;
+                    RaisePropertyChanged("QuestionAnswer");
+                }
+            }
+        }
+        public int QuestionAnswerPoints
+        {
+            get { return questionAnswerPoints; }
+            set
+            {
+                if (questionAnswerPoints != value)
+                {
+                    questionAnswerPoints = value;
+                    RaisePropertyChanged("QuestionAnswerPoints");
+                }
+            }
+        }
+        #endregion
+
+        public class ScaleAnswersQuestion : ObservableCollection<ScaleAnswers>
+        {
+            private string questionName;
+            public string QuestionName
+            {
+                get { return questionName; }
+                set
+                {
+                    if (questionName != value)
+                    {
+                        questionName = value;
+                    }
+                }
+            }
+
+
+        }
+
+
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        #endregion
+
+    }
+
+}
--- Skale_W_Praktyce/Skale_W_Praktyce/ViewModels/ScalesViewModel.cs
+++ Skale_W_Praktyce/Skale_W_Praktyce/ViewModels/ScalesViewModel.cs
@@ -9,6 +9,8 @@ using System.Runtime.CompilerServices;
 using Skale_W_Praktyce.Views;
 using System.Linq;
 using Skale_W_Praktyce.Views.Scales;
+using System.Collections.Generic;
+using static Skale_W_Praktyce.Models.ScaleAnswers;
 
 namespace Skale_W_Praktyce.ViewModels
 {
@@ -35,7 +37,7 @@ namespace Skale_W_Praktyce.ViewModels
                         ScaleName = "Skala śpiączki Glasgow (dla dorosłych)",
                         ScaleDesc = "GLASGOW Meningococcal Septicemia Prognostic Score (GMSPS).",
                         ScaleTags = "Układ nerwowy",
-                        ScaleViewName = typeof(MainPage_Flyout)},
+                        ScaleViewName = typeof(scale_GLASGOW)},
                     new Scale(){
                         ScaleName = "Skala TINETTI",
                         ScaleDesc = "Służy do oceny równowagi i chodu.",
@@ -82,11 +84,64 @@ namespace Skale_W_Praktyce.ViewModels
                         ScaleTags = "Ogólne, Psychiatria",
                         ScaleViewName = typeof(MainPage_Flyout)},
                 };
+
+
+            #region InScalesList - questions and answers
+            ScaleQuestions = new ObservableCollection<ScaleAnswersQuestion>();
+            var que1 = new ScaleAnswersQuestion() { QuestionName = "Otwieranie oczu"};
+            que1.Add(new ScaleAnswers() { QuestionAnswer = "spontaniczne", QuestionAnswerPoints = 1 });
+            que1.Add(new ScaleAnswers() { QuestionAnswer = "na polecenie", QuestionAnswerPoints = 2 });
+            que1.Add(new ScaleAnswers() { QuestionAnswer = "na bodźce bólowe", QuestionAnswerPoints = 2 });
+            que1.Add(new ScaleAnswers() { QuestionAnswer = "nie otwiera oczu", QuestionAnswerPoints = 2 });
+
+
+            var que2 = new ScaleAnswersQuestion() { QuestionName = "Kontakt słowny" };
+            que2.Add(new ScaleAnswers() { QuestionAnswer = "odpowiedź logiczna, pacjent zorientowany co do miejsca, czasu i własnej osoby", QuestionAnswerPoints = 3 });
+            que2.Add(new ScaleAnswers() { QuestionAnswer = "odpowiedź splątana, pacjent zdezorientowany", QuestionAnswerPoints = 1 });
+            que2.Add(new ScaleAnswers() { QuestionAnswer = "odpowiedź nieadekwatna, nie na temat lub krzyk", QuestionAnswerPoints = 3 });
+            que2.Add(new ScaleAnswers() { QuestionAnswer = "niezrozumiałe dźwięki, pojękiwanie", QuestionAnswerPoints = 1 });
+            que2.Add(new ScaleAnswers() { QuestionAnswer = "bez reakcji", QuestionAnswerPoints = 3 });
+
+            var que3 = new ScaleAnswersQuestion() { QuestionName = "Kontakt jakis" };
+            que3.Add(new ScaleAnswers() { QuestionAnswer = "odpowiedź logiczna, pacjent zorientowany co do miejsca, czasu i własnej osoby", QuestionAnswerPoints = 3 });
+            que3.Add(new ScaleAnswers() { QuestionAnswer = "odpowiedź splątana, pacjent zdezorientowany", QuestionAnswerPoints = 1 });
+            que3.Add(new ScaleAnswers() { QuestionAnswer = "odpowiedź nieadekwatna, nie na temat lub krzyk", QuestionAnswerPoints = 3 });
+            que3.Add(new ScaleAnswers() { QuestionAnswer = "niezrozumiałe dźwięki, pojękiwanie", QuestionAnswerPoints = 1 });
+            que3.Add(new ScaleAnswers() { QuestionAnswer = "bez reakcji", QuestionAnswerPoints = 3 });
+
+
+            ScaleQuestions.Add(que1);
+            ScaleQuestions.Add(que2);
+            ScaleQuestions.Add(que3);
+
+            //    List<ScaleAnswers> Groups = new List<ScaleAnswers> {
+            //    new PageTypeGroup ("Alpha", "A"){
+            //        new PageModel("Amelia", "Cedar", new switchCellPage(),""),
+            //        new PageModel("Alfie", "Spruce", new switchCellPage(), "grapefruit.jpg"),
+            //        new PageModel("Ava", "Pine", new switchCellPage(), "grapefruit.jpg"),
+            //        new PageModel("Archie", "Maple", new switchCellPage(), "grapefruit.jpg")
+            //    },
+            //    new PageTypeGroup ("Bravo", "B"){
+            //        new PageModel("Brooke", "Lumia", new switchCellPage(),""),
+            //        new PageModel("Bobby", "Xperia", new switchCellPage(), "grapefruit.jpg"),
+            //        new PageModel("Bella", "Desire", new switchCellPage(), "grapefruit.jpg"),
+            //        new PageModel("Ben", "Chocolate", new switchCellPage(), "grapefruit.jpg")
+            //    }
+            //};
+            //    All = Groups; //set the publicly accessible list
+            #endregion
+
+
         }
+
+
+
 
         #region Fields
         private ObservableCollection<Scale> scalesList;
         private ObservableCollection<Scale> scalesListCategory;
+        public ObservableCollection<ScaleAnswersQuestion> scaleQuestions;
+
         #endregion
 
         #region Properties
@@ -102,7 +157,6 @@ namespace Skale_W_Praktyce.ViewModels
                 }
             }
         }
-
         public ObservableCollection<Scale> ScalesListCategory
         {
             get { return scalesListCategory; }
@@ -115,7 +169,18 @@ namespace Skale_W_Praktyce.ViewModels
                 }
             }
         }
-
+        public ObservableCollection<ScaleAnswersQuestion> ScaleQuestions
+        {
+            get { return scaleQuestions; }
+            set
+            {
+                if (scaleQuestions != value)
+                {
+                    scaleQuestions = value;
+                    OnPropertyChanged("ScaleQuestions");
+                }
+            }
+        }
         #endregion
 
         #region Commands
