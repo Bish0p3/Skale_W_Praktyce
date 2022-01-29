830d4e7fb8877556b373c77a45cd502b1b889317
Borys Więsek <lordboria.boryson@gmail.com>
2022-01-29 20:17:51 +0100

    Changed NAVBAR color, Changed STATUSBAR color

--- Skale_W_Praktyce/Skale_W_Praktyce.Android/MainActivity.cs
+++ Skale_W_Praktyce/Skale_W_Praktyce.Android/MainActivity.cs
@@ -16,6 +16,8 @@ namespace Skale_W_Praktyce.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            Window.SetStatusBarColor(Android.Graphics.Color.Argb(255, 240, 113, 103)); //secondary
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
